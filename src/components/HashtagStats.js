9ab4229f892bdd5435431abda10f5fbd73c42df2
Marc Farra <marcfarra@gmail.com>
2016-01-26 17:06:27 -0500

    Make checkboxes smart

--- src/components/HashtagStats.js
+++ src/components/HashtagStats.js
@@ -4,14 +4,52 @@ import Toggle from '../components/ToggleSwitch.js';
 import Chart from '../components/HashtagChart.js';
 
 export default React.createClass({
+  getInitialState: function () {
+    return {
+      filters: [],
+      hashtags: {},
+      colors: {}
+    };
+  },
+  assignColors: function (props) {
+    var colorClasses = ['hashtag1', 'hashtag2', 'hashtag3'];
+    var colors = R.zipObj(Object.keys(props.hashtags), colorClasses);
+    this.setState({
+      colors: colors
+    });
+  },
+  componentDidMount: function () {
+    this.setState({
+      hashtags: this.props.hashtags
+    });
+    this.assignColors(this.props);
+  },
+  componentWillReceiveProps: function (nextProps) {
+    this.setState({
+      hashtags: nextProps.hashtags
+    });
+    this.assignColors(nextProps);
+  },
   handleChange: function (toggles) {
-    console.log(toggles);
+    this.setState({
+      filters: toggles
+    });
   },
   render: function () {
     var props = this.props;
-    var hashtags = props.hashtags;
+    var component = this;
+
+    var hashtags = R.pickBy(function (val, key) {
+      return !R.contains(key, component.state.filters);
+    }, props.hashtags);
     var list = R.map(R.nth(1), R.toPairs(R.map(R.prop('total'), hashtags)));
-    var totals = list.reduce(R.mergeWith((a, b) => a + b), {});
+    var totals = list.reduce(R.mergeWith((a, b) => a + b), {
+      buildings: 0,
+      waterways: 0,
+      pois: 0,
+      roads: 0
+    });
+
     return (
       <div className='Leaderboard-Stats-Container'>
         <div className='Hashtag-Timeline-Container'>
@@ -19,7 +57,7 @@ export default React.createClass({
         </div>
         <div className='Stats-Content'>
           <div className = 'Stats-Togglebox'>
-            <Toggle onChange={this.handleChange} />
+            <Toggle onChange={this.handleChange} hashtags={this.state.colors}/>
           </div>
           <div className = 'Stats-Statbox'>
             <table className = "table-curve">
--- src/components/ToggleSwitch.js
+++ src/components/ToggleSwitch.js
@@ -1,29 +1,32 @@
 import React from 'react';
+import CheckboxGroup from 'react-checkbox-group';
 
-export default function () {
-	return (
-		<div>
-			<div className="onoffswitch">
-				<input type="checkbox" name="onoffswitch" className="onoffswitch-checkbox" id="myonoffswitch" ></input>
-					<label className="onoffswitch-label" htmlFor="myonoffswitch">
-					<span className="onoffswitch-inner"></span>
-					<span className="onoffswitch-switch "></span>
-				</label>
-			</div>
-			<div className="onoffswitch">
-				<input type="checkbox" name="onoffswitch2" className="onoffswitch-checkbox" id="myonoffswitch2" ></input>
-					<label className="onoffswitch-label" htmlFor="myonoffswitch2">
-					<span className="onoffswitch-inner"></span>
-					<span className="onoffswitch-switch hashtag2"></span>
-				</label>
-			</div>
-			<div className="onoffswitch">
-				<input type="checkbox" name="onoffswitch3" className="onoffswitch-checkbox" id="myonoffswitch3" ></input>
-					<label className="onoffswitch-label" htmlFor="myonoffswitch3">
-					<span className="onoffswitch-inner"></span>
-					<span className="onoffswitch-switch hashtag3"></span>
-				</label>
-			</div>
-		</div>
-	);
-}
+export default React.createClass({
+  handleChange: function (key) {
+    this.props.onChange(this.refs.checkboxes.getCheckedValues());
+  },
+  render: function () {
+    var props = this.props;
+    if (!props.hashtags) {
+      return <div></div>;
+    }
+    var toggles = Object.keys(props.hashtags).map((key, index) => {
+      var className = 'onoffswitch-switch ' + props.hashtags[key];
+      return (
+          <label className="onoffswitch" key={key}>
+            <input type="checkbox" className="onoffswitch-checkbox" value={key} />
+            <div className="onoffswitch-label">
+              <span className="onoffswitch-inner"></span>
+              <span className={className}></span>
+            </div>
+          </label>
+      );
+    });
+    return (
+      <CheckboxGroup
+        ref="checkboxes" onChange={this.handleChange}>
+        {toggles}
+      </CheckboxGroup>
+    );
+  }
+});
