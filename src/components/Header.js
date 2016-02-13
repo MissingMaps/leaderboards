ab73ed2e7190c7573b99aa80ec0d59c8413288d6
Dylan <Dylan@developmentseed.org>
2016-02-12 22:19:13 -0500

    Getting header consistent across MM sites

--- src/components/Header.js
+++ src/components/Header.js
@@ -4,17 +4,21 @@ export default function () {
   return (
 		<header>
 			<div className="navbar-background">
-				<div className="nav-icon"><img src="../assets/graphics/MissingMapsLogo-White.svg" width="94px"></img></div>
+				<div className="nav-icon">
+					<a href="{{site.baseurl}}/">
+						<img src="../assets/graphics/MissingMapsLogo-White.svg" width="94px"></img>
+					</a>
+				</div>
 				<div className="nav-list">
 					<ul>
-						<li className="nav-item"><a href="">CONTRIBUTE</a></li>
-						<li className="nav-item"><a href="">EVENTS</a></li>
-						<li className="nav-item"><a href="">ABOUT</a></li>
-						<li className="nav-item nav-dropdown"><a href="">DISCOVER</a>
-						<div className="dropdown-content">
-							<div className="nav-item"><a href="">USER PAGES</a></div>
-							<div className="nav-item"><a href="">LEADERBOARDS</a></div>
-						</div>
+						<a href=""><li className="nav-item">CONTRIBUTE</li></a>
+						<a href=""><li className="nav-item">EVENTS</li></a>
+						<a href=""><li className="nav-item">ABOUT</li></a>
+						<li className="nav-item nav-dropdown">DISCOVER
+							<div className="dropdown-content">
+								<a href=""><div className="nav-item">USER PROFILES</div></a>
+								<a href=""><div className="nav-item">LEADERBOARDS</div></a>
+							</div>
 						</li>
 					</ul>
 				</div>
@@ -22,11 +26,11 @@ export default function () {
 			<div className="resp-navbar navbar-background resp-nav-dropdown">
 				<p>MENU</p>
 				<div className="resp-dropdown-content">
-					<div className="nav-item"><a href="">CONTRIBUTE</a></div>
-					<div className="nav-item"><a href="">EVENTS</a></div>
-					<div className="nav-item"><a href="">ABOUT</a></div>
-					<div className="nav-item"><a href="">USER PAGES</a></div>
-					<div className="nav-item"><a href="">LEADERBOARDS</a></div>
+					<a href=""><li className="nav-item">CONTRIBUTE</li></a>
+					<a href=""><li className="nav-item">EVENTS</li></a>
+					<a href=""><li className="nav-item">ABOUT</li></a>
+					<a href=""><li className="nav-item">USER PROFILES</li></a>
+					<a href=""><li className="nav-item">LEADERBOARDS</li></a>
 				</div>
 			</div>
 		</header>
