a1e7304e35e1d2a5bc8b69e2694d3b5b2c9f0a8f
Dylan <Dylan@developmentseed.org>
2016-02-15 11:29:45 -0500

    Add footer, make it in line with other properties

--- src/components/Leaderboard.js
+++ src/components/Leaderboard.js
@@ -2,6 +2,7 @@ import React from 'react';
 import {Cell, Table, Column} from 'fixed-data-table';
 import R from 'ramda';
 import moment from 'moment';
+import Footer from '../components/Footer.js';
 
 var SortTypes = {
   ASC: 'ASC',
@@ -277,6 +278,7 @@ export default React.createClass({
         </div>
           </div>
         </section>
+        <Footer />
       </div>
     );
   }
