1081c561c0f91c8e7a091383d5c217c719c77d48
Dylan <Dylan@developmentseed.org>
2016-02-19 17:41:47 -0500

    Styled

--- src/components/ComparisonBar.js
+++ src/components/ComparisonBar.js
@@ -1,12 +1,13 @@
 import React from 'react';
 
 export default function (props) {
-  var color = props.totals.color + " comparison-bar";
+  var color =  "comparison-bar "+ props.totals.color;
   var edits = props.totals.edits;
+  var textcolor = props.totals.color+"-proj comparison-number";
   var percentage = edits * 100.0 / props.max;
 
   return <div className = "comparison-card">
-    <div className = "comparison-number">
+    <div className = {textcolor}>
       {props.hashtag}
     </div>
     <div className = "comparison-bar-container">
