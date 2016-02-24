58223c889fc276108e90eda36774f47b6b9f2ba0
smit1678 <nate@developmentseed.org>
2016-02-24 20:12:05 +0000

    remove old cname, change directory on travis

--- .build_scripts/deploy.sh
+++ .build_scripts/deploy.sh
@@ -4,7 +4,7 @@ set -e # halt script on error
 # If this is the deploy branch, push it up to gh-pages
 if [ $TRAVIS_PULL_REQUEST = "false" ] && [ $TRAVIS_BRANCH = ${DEPLOY_BRANCH} ]; then
   echo "Get ready, we're pushing to gh-pages!"
-  cd _site
+  cd dist
   git init
   git config user.name "Travis-CI"
   git config user.email "travis@somewhere.com"
