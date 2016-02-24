a4152b0c6a00be9a8f054d956fb34efe484f1e97
Dylan <Dylan@developmentseed.org>
2016-02-24 15:06:32 -0500

    Sets links for MissingMaps instead of staging

--- src/components/Header.js
+++ src/components/Header.js
@@ -5,19 +5,19 @@ export default function () {
     <header> 
       <div className="navbar-background">
         <div className="nav-icon">
-          <a href="http://missingmaps-demo.devseed.com/">
+          <a href="http://missingmaps.org/">
             <img src="./assets/graphics/MissingMapsLogo-White.svg" width="94px"></img>
           </a>
         </div>
         <div className="nav-list">
           <ul>
-            <a href="http://missingmaps-demo.devseed.com/contribute/"><li className="nav-item">CONTRIBUTE</li></a>
-            <a href="http://missingmaps-demo.devseed.com/events/"><li className="nav-item">EVENTS</li></a>
-            <a href="http://missingmaps-demo.devseed.com/about/"><li className="nav-item">ABOUT</li></a>
+            <a href="http://missingmaps.org/contribute/"><li className="nav-item">CONTRIBUTE</li></a>
+            <a href="http://missingmaps.org/events/"><li className="nav-item">EVENTS</li></a>
+            <a href="http://missingmaps.org/about/"><li className="nav-item">ABOUT</li></a>
             <li className="nav-item nav-dropdown">DISCOVER
               <div className="dropdown-content">
-                <a href="http://missingmaps-users-demo.devseed.com/"><div className="nav-item">USER PROFILES</div></a>
-                <a href="http://missingmaps-leaderboards-demo.devseed.com/"><div className="nav-item">LEADERBOARDS</div></a>
+                <a href="http://missingmaps.org/users"><div className="nav-item">USER PROFILES</div></a>
+                <a href="http://missingmaps.org/leaderboards"><div className="nav-item">LEADERBOARDS</div></a>
               </div>
             </li>
           </ul>
@@ -26,11 +26,11 @@ export default function () {
       <div className="resp-navbar navbar-background resp-nav-dropdown">
         <p>MENU</p>
         <div className="resp-dropdown-content">
-          <a href="http://missingmaps-demo.devseed.com/contribute/"><li className="nav-item">CONTRIBUTE</li></a>
-          <a href="http://missingmaps-demo.devseed.com/events/"><li className="nav-item">EVENTS</li></a>
-          <a href="http://missingmaps-demo.devseed.com/about/"><li className="nav-item">ABOUT</li></a>
-          <a href="http://missingmaps-users-demo.devseed.com"><li className="nav-item">USER PROFILES</li></a>
-          <a href="http://missingmaps-leaderboards-demo.devseed.com"><li className="nav-item">LEADERBOARDS</li></a>
+          <a href="http://missingmaps.org/contribute/"><li className="nav-item">CONTRIBUTE</li></a>
+          <a href="http://missingmaps.org/events/"><li className="nav-item">EVENTS</li></a>
+          <a href="http://missingmaps.org/about/"><li className="nav-item">ABOUT</li></a>
+          <a href="http://missingmaps.org/users"><li className="nav-item">USER PROFILES</li></a>
+          <a href="http://missingmaps.org/leaderboards"><li className="nav-item">LEADERBOARDS</li></a>
         </div>
       </div>
     </header>
