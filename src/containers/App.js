3f55b08ea7b68f079cec0eee177fce5b3edb22aa
Seth Fitzsimmons <seth@mojodna.net>
2017-11-27 12:13:30 -0800

    Use Azure osm-stats-api endpoints

--- src/containers/App.js
+++ src/containers/App.js
@@ -13,7 +13,8 @@ import "../assets/styles/table-osm.css";
 import "../assets/styles/main.css";
 
 const STATS_API_URL =
-  process.env.REACT_APP_STATS_API_URL || "https://osmstats.redcross.org";
+  process.env.REACT_APP_STATS_API_URL ||
+  "https://osm-stats-api.azurewebsites.net";
 
 const get = async uri => {
   const rsp = await fetch(uri);
