e3c026e54a3c737618749d11bca18cf10bb780f2
Seth Fitzsimmons <seth@mojodna.net>
2018-02-18 19:49:32 -0800

    Update CC-BY year(s)

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,15 +1,15 @@
-import React from 'react';
+import React from "react";
 
-export default function () {
+export default function() {
   return (
-	<div className ="wrapper">
-		<div className ="footer-container">
-			<div className ="center-text">
-				<div className ="footer-mm center-text">
-				CC-BY Missing Maps 2016
-				</div>
-			</div>
-		</div>
-	</div>
+    <div className="wrapper">
+      <div className="footer-container">
+        <div className="center-text">
+          <div className="footer-mm center-text">
+            CC-BY Missing Maps 2016-2018
+          </div>
+        </div>
+      </div>
+    </div>
   );
 }
